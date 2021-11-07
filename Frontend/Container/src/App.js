cb61a665d91afdebd754e31333eec4de63026d70
ramumtaz <rashid@mentoringminds.com>
2021-11-07 14:48:20 +0500

    Fix random genrated classnames in react
    closes#14

--- Frontend/Container/src/App.js
+++ Frontend/Container/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
-import header from './components/Header';
+import { StylesProvider,createGenerateClassName } from '@material-ui/styles';
 import Header from './components/Header';
+
+const generateClassName=createGenerateClassName({
+  productionPrefix:'co'
+});
 // hi there
 export default () => {
   return (
     <BrowserRouter>
+    <StylesProvider>
     <div>
       <Header/>
       <MarketingApp />
     </div>
+    </StylesProvider>
     </BrowserRouter>
   );
 };
